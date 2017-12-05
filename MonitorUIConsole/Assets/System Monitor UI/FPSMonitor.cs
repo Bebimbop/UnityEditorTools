a95c31598748ac7c85fbb0f76a8aa6d0a7443a6c
Kyle Dittle <kyledittle@gmail.com>
2017-12-05 09:30:28 -0600

    Added monitor GUI **WIP

---
+++ MonitorUIConsole/Assets/System Monitor UI/FPSMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSMonitor : MonoBehaviour
+{
+    [Tooltip("Rate at which the FPS will update.")]
+    public float updateRate = 1.0f;
+
+    public Text FPS_Text;
+
+    private uint frameCount = 0;
+    private double deltaTime = 1;
+    private double fps;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    if (FPS_Text != null)
+	        FPS_Text.text = (frameCount / deltaTime).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    frameCount++;
+	    deltaTime += Time.deltaTime;
+
+	    if (deltaTime > 1.0 / updateRate)
+	    {
+	        fps = (frameCount / deltaTime);
+	        frameCount = 0;
+	        deltaTime -= 1.0 / updateRate;
+	    }
+
+
+	    FPS_Text.text = string.Format("{0:0.00}", fps);
+    }
+}
---
+++ MonitorUIConsole/Assets/System Monitor UI/GPUMonitor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using UnityEngine.UI;
+
+public class GPUMonitor : MonoBehaviour
+{
+    public Text GPU_Type;
+    public Text GPU_DeviceVersion;
+    public Text GPU_MemorySize;
+    public Text GPU_MemoryUsed;
+
+    private PerformanceCounter _gpuCounter;
+    private float _curMemoryUsed;
+
+	// Use this for initialization
+	void Start ()
+	{
+        _gpuCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+	    if (GPU_Type != null)
+	        GPU_Type.text = SystemInfo.graphicsDeviceName;
+
+	    if (GPU_DeviceVersion != null)
+	        GPU_DeviceVersion.text = SystemInfo.graphicsDeviceVersion;
+
+	    if (GPU_MemorySize != null)
+	        GPU_MemorySize.text = SystemInfo.graphicsMemorySize.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _curMemoryUsed = _gpuCounter.NextValue();
+
+	    if (GPU_MemoryUsed != null)
+	        GPU_MemoryUsed.text = (_curMemoryUsed / SystemInfo.graphicsMemorySize).ToString() + "%";
+	}
+}
